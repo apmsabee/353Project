309c29f262f23881920577137009961e8344170f
Andy Sabee <65576827+droodhub@users.noreply.github.com>
2023-05-02 09:11:35 -0500

    Bitmaps for our onscreen icons

---
+++ meteor.h
@@ -0,0 +1,5 @@
+// Bitmap info for actualmeteor
+extern const uint_8 actualmeteorBitmaps;
+extern const uint_8 actualmeteorWidthPixels;
+extern const uint_8 actualmeteorHeightPixels;
+
---
+++ person.h
@@ -0,0 +1,5 @@
+// Bitmap info for stickfigurestickmaniconpictogramGraphics125184071580x387
+extern const uint_8 stickfigurestickmaniconpictogramGraphics125184071580x387Bitmaps;
+extern const uint_8 stickfigurestickmaniconpictogramGraphics125184071580x387WidthPixels;
+extern const uint_8 stickfigurestickmaniconpictogramGraphics125184071580x387HeightPixels;
+
