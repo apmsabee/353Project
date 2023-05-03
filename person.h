fd4e8245175e08ba651e3f6f4e48b3ee3884a43f
Andy Sabee <65576827+droodhub@users.noreply.github.com>
2023-05-03 15:20:03 -0500

    Delete person.h

--- person.h
+++
@@ -1,5 +0,0 @@
-// Bitmap info for stickfigurestickmaniconpictogramGraphics125184071580x387
-extern const uint_8 stickfigurestickmaniconpictogramGraphics125184071580x387Bitmaps;
-extern const uint_8 stickfigurestickmaniconpictogramGraphics125184071580x387WidthPixels;
-extern const uint_8 stickfigurestickmaniconpictogramGraphics125184071580x387HeightPixels;
-
