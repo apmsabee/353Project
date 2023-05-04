b23cf1700d7a8c293c37684e43ba1df8af57163c
cyschlueter <91997798+cyschlueter@users.noreply.github.com>
2023-05-03 20:49:46 -0500

    Add files via upload
    Buzzer is working.

--- ece353_final_project/task_buzzer.c
+++ ece353_final_project/task_buzzer.c
@@ -5,11 +5,24 @@
  *      Author: apmcavoy
  */
 #include "task_buzzer.h"
+#include "music.h"
 TaskHandle_t Task_Buzzer_Handle;
 
 void Task_Buzzer(void *pvParameters){
-    while(1){
-        //just play the song, this task should only be called if the player dies
+    uint32_t player_died;
+    while(1) {
+        //TODO: you need to GIVE this notification when a player dies
+        player_died = ulTaskNotifyTake(true, portMAX_DELAY);
+        music_play_song();
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
 
+void initBuzzer() {
+    //Initialize Buzzer
+        //initialize buzzer as output, set as primary function
+        P2->DIR |= BIT7;
+        P2->SEL0 |= BIT7;
+        P2->SEL1 &= ~BIT7;
+}
